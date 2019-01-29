c55c3c2eb6ac7e15b5a9d96f0bf1ca5bac01d165
Bianca N <bianca.neptune@gmail.com>
2019-01-29 11:44:44 -0500

    we have specs

--- wrap.js
+++ wrap.js
@@ -1,5 +1,15 @@
 const wrap = (string, column) => {
-    if (!string) return ''
-}
+  if (!string) return "";
+  if (string.length <= column) {
+    return string;
+  }
+  if (string.length > column) {
+    let frontSlice = string.slice(0, column);
+    let lastSpace = frontSlice.lastIndexOf(" ");
+    let endSlice = string.slice(lastSpace + 1);
+    let returnedStr = frontSlice.slice(0, lastSpace) + "\n" + endSlice;
+    return returnedStr;
+  }
+};
 
-module.exports = wrap
+module.exports = wrap;
--- wrap.spec.js
+++ wrap.spec.js
@@ -1,8 +1,16 @@
-const {expect} = require('chai')
-const wrap = require('./wrap')
+const { expect } = require("chai");
+const wrap = require("./wrap");
 
-describe('wrap', () => {
-    it ('Returns an empty string if empty string is passed in', () => {
-        expect(wrap('', 10)).to.equal("")
-    })
-})
+describe("wrap", () => {
+  it("Returns an empty string if empty string is passed in", () => {
+    expect(wrap("", 10)).to.equal("");
+  });
+
+  it("Returns string if column is greater than string length", () => {
+    expect(wrap("Hello", 7)).to.equal("Hello");
+  });
+
+  it("Returns string with breaks if column is greater than string length", () => {
+    expect(wrap("Hello world", 7)).to.equal("Hello\nworld");
+  });
+});
